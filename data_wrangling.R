a109409be32e42ebd337002fc87d41f9b832a0aa
damien-dupre <damien.dupre.ac@gmail.com>
2019-03-28 18:32:54 +0000

    db_bigquery

--- data_wrangling.R
+++ data_wrangling.R
@@ -1,23 +1,41 @@
 
 # libraries --------------------------------------------------------------------
 library(tidyverse)
+library(bigrquery)
 library(janitor)
 library(lubridate)
+library(plotly)
+# database ---------------------------------------------------------------------
+con <- DBI::dbConnect(bigquery(),
+                      project = config::get("dcu_bigquery")$project,
+                      dataset = config::get("dcu_bigquery")$dataset,
+                      billing = config::get("dcu_bigquery")$billing
+)
+#DBI::dbListTables(con)
+bitcoin_blockchain <- con %>% tbl("bitcoin_blockchain")
 # data -------------------------------------------------------------------------
-data_raw <- readr::read_csv("data/bitcoin_part_1.csv") %>% 
+data_raw <- bitcoin_blockchain %>%
+  dplyr::select(id,actor_postedTime) %>% 
   dplyr::filter(!is.na(id)) %>%
-  janitor::remove_empty("cols") %>% 
-  janitor::clean_names() %>% 
-  dplyr::mutate(actor_posted_date = as.Date(actor_posted_time)) %>% 
+  dplyr::filter(!is.na(actor_postedTime)) %>%
+  collect()
+# data -------------------------------------------------------------------------
+data_raw <- data_raw %>% 
+  janitor::remove_empty("cols") %>%
+  janitor::clean_names() %>%
+  dplyr::mutate(actor_posted_date = as.Date(actor_posted_time))%>% 
   dplyr::mutate(n = 1)
 
+first_date <- min(data_raw$actor_posted_date)
+last_date <- max(data_raw$actor_posted_date)
+# data -------------------------------------------------------------------------
 # WARNING: doesn't take timezones into account
 data_day <- data_raw %>% 
   dplyr::group_by(day = lubridate::floor_date(actor_posted_date, "day")) %>%
   dplyr::summarize(amount = sum(n)) %>%
-  tidyr::complete(day = seq.Date(min(data_raw$actor_posted_date), max(data_raw$actor_posted_date), by="day")) %>% 
+  tidyr::complete(day = seq.Date(first_date, last_date, by="day")) %>% 
   dplyr::mutate(amount = tidyr::replace_na(amount, 0))
-
+# data -------------------------------------------------------------------------
 plot_day <- data_day %>%
   ggplot(aes(day, amount)) + 
   geom_point() +
@@ -26,6 +44,13 @@ plot_day <- data_day %>%
   scale_x_date("Time") +
   scale_y_continuous("N tweet")
 
-library(plotly)
-
 ggplotly(plot_day)
+
+# SQL --------------------------------------------------------------------------
+sql <- "SELECT id, DATE(actor_postedTime) AS date_tweet, count(*) AS n
+        FROM `stable-plasma-690.Bitcoin_Blockchain.bitcoin_blockchain`
+        GROUP BY id, DATE(actor_postedTime);"
+
+tb <- bq_project_query(config::get("dcu_bigquery")$billing, sql)
+
+bq_table_download(tb, max_results = 10)
